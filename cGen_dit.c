70e6a4da20574eed60fad68c20709d0cbcd57319
Ian Page Hands <iphands@handsbox.localdomain>
2009-07-05 12:58:39 -0400

    Added GPL message.

--- cGen_dit.c
+++ cGen_dit.c
@@ -1,3 +1,19 @@
+/*
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** Any non-GPL usage of this software or parts of this software is strictly
+** forbidden.
+**
+*/
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -80,7 +96,8 @@ int main(int argc, char *argv[])
 
 
   // Seed rand with time
-  unsigned int seed = (unsigned int)time(NULL);
+  //unsigned int seed = (unsigned int)time(NULL);
+  unsigned int seed = 1111;
   srand(seed);
 
   // Initialize string storage, and pass to get_randline
